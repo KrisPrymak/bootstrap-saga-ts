c6928535fc41c662427265c5823c635c1e4c734e
KrisPrymak <kriskiwinkaa@gmail.com>
2023-05-22 22:21:32 +0300

    getPostsSaga

--- src/store/postsSlice.ts
+++ src/store/postsSlice.ts
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import { delay, put } from 'redux-saga/effects'
+import { postsAPI } from "../api/api";
 
 export interface IPostItem {
   userId: number;
@@ -17,10 +19,29 @@ const initialState: IPostState = {
   loading: false,
 };
 
+export function* getPostsSaga(): any {
+    yield put(isLoading(true))
+    yield delay(500)
+    const response = yield postsAPI.getPosts();
+    yield put(getPostsSuccess(response.data));
+    yield put(isLoading(false))
+  }
+
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    getPostsSuccess: (state, action) => {
+          state.list = action.payload;
+      },
+    isLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+  },
 });
 
+export const GET_POSTS = 'posts/getPosts';
+export const getPosts = createAction(GET_POSTS)
+
+export const { getPostsSuccess, isLoading } = postsSlice.actions;
 export default postsSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from 'redux-saga';
-import postsSlice from './postsSlice';
+import postsSlice, { GET_POSTS, getPostsSaga } from './postsSlice';
+import { takeEvery } from "redux-saga/effects";
 
 const sagaMiddleware = createSagaMiddleware();
 
 function* sagas() {
-
+    yield takeEvery(GET_POSTS, getPostsSaga)
 }
 
 export const store = configureStore({
