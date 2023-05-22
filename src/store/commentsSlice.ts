597df59936509e5b721e3dec54dd7802675e31e8
KrisPrymak <kriskiwinkaa@gmail.com>
2023-05-22 22:29:44 +0300

    getCommentsSaga

--- src/store/commentsSlice.ts
+++ src/store/commentsSlice.ts
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import { AxiosResponse } from "axios";
+import { delay, put } from "redux-saga/effects";
+import { commentsAPI } from "../api/api";
 
 export interface ICommentItem {
   postId: number
@@ -8,8 +11,13 @@ export interface ICommentItem {
   body: string
 }
 
+interface ICommentObject {
+  postId: number
+  contentList: Array<ICommentItem>
+}
+
 interface ICommentsState {
-  list: Array<ICommentItem>;
+  list: Array<ICommentObject>;
   loading: boolean;
 }
 
@@ -18,10 +26,34 @@ const initialState: ICommentsState = {
   loading: false,
 };
 
+export function* getCommentsSaga(payload: any) {
+  yield put(isLoading(true))
+  yield delay(500)
+  const response: AxiosResponse = yield commentsAPI.getComments(payload.payload);
+  yield put(getCommentsSuccess(response.data));
+  yield put(isLoading(false))
+}
+
 const commentsSlice = createSlice({
   name: "comments",
   initialState,
-  reducers: {},
+  reducers: {
+    getCommentsSuccess: (state, action) => {
+      const index = state.list.findIndex(item => item.postId === (action.payload[0]).postId)
+      index === -1 &&(
+      state.list.push({
+        postId: (action.payload)[0].postId,
+        contentList: action.payload
+      }))
+    },
+    isLoading: (state, action) => {
+      state.loading = action.payload;
+    }
+  },
 });
 
+export const GET_COMMENTS = 'comments/getComments';
+export const getComments = createAction(GET_COMMENTS);
+
+export const { getCommentsSuccess, isLoading } = commentsSlice.actions;
 export default commentsSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,16 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from 'redux-saga';
 import postsSlice, { GET_POSTS, getPostsSaga } from './postsSlice';
 import { takeEvery } from "redux-saga/effects";
+import commentsSlice, { GET_COMMENTS, getCommentsSaga } from "./commentsSlice";
+import userSlice from './userSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 function* sagas() {
     yield takeEvery(GET_POSTS, getPostsSaga)
+    yield takeEvery(GET_COMMENTS, getCommentsSaga)
 }
 
 export const store = configureStore({
     reducer: {
         postsSlice,
+        commentsSlice,
+        userSlice,
     }, 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({thunk: false}).concat(sagaMiddleware)
 });
