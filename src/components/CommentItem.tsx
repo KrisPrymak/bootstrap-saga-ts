6cae786cb7ddb455452fc4204c8231bda24d43db
KrisPrymak <kriskiwinkaa@gmail.com>
2023-05-23 20:30:12 +0300

    commentItem

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
-const CommentItem = () => {
+interface ICommentItemProps {
+    comment: {
+        body: string
+        email: string
+        id: number
+        name: string
+        postId: number
+    }
+}
+
+const CommentItem: React.FC<ICommentItemProps> = ({ comment }) => {
     return (
-        <div>
-            
-        </div>
+        <Card style={{ width: "80%" }} className='mb-3'>
+            <Card.Body>
+                <Card.Title>{comment.email}</Card.Title>
+                <Card.Text>{comment.body}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 };
 
