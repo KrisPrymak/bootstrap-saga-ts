3ed0a8b656da3903669b489cd7a34dbb8997e41d
KrisPrymak <kriskiwinkaa@gmail.com>
2023-05-23 20:27:41 +0300

    aboutme

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import UserInfoCard from './UserInfoCard';
+import { IUser } from '../store/userSlice';
 
 const AboutMe = () => {
+    const myInfo: IUser = {
+        id: 109071997,
+        name: 'Кристина Приймак',
+        username: 'KrisPriimak',
+        email: 'kris.priimak.frontend@gmail.com',
+        address: {
+            street: '',
+            suite: '',
+            city: 'Russia',
+            zipcode: '400055',
+            geo: {
+                lat: '',
+                lng: '',
+            },
+        },
+        phone: '+79178849863',
+        website: 'github.com/KrisPrymak',
+        company: {
+            name: '',
+            catchPhrase: '',
+            bs: '',
+        }
+    }
+    
     return (
-        <div>
-            AboutMe
-        </div>
+        <UserInfoCard currentUser={myInfo} />
     );
 };
 
